c57fa1a1336a75dfec4f9c7346e78b6e1097f409
Erik Klein <erik@audiotrap.com>
2019-10-30 20:49:28 -0700

    adding full db info.

--- schema/user.sql
+++ schema/user.sql
@@ -1,6 +1,10 @@
+CREATE DATABASE churrios;
+USE DATABASE churrios;
 CREATE TABLE user 
 (
 	uuid VARCHAR(50) NOT NULL PRIMARY KEY,
 	name VARCHAR(100),
 	email VARCHAR(100)
 );
+
+insert into user values ("eb8e8fa4-1e04-4aa5-bd7e-f44480e0a231", "Erik Klein" , "erik@audiotrap.com");
